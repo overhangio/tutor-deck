a4b046db07f3fcc0dba2192a7cc5e833c5dc3ae5
Régis Behmo <regis@behmo.com>
2024-12-05 17:28:51 +0100

    feat: store tutor logs in temporary files

--- tutordash/server/app.py
+++ tutordash/server/app.py
@@ -1,15 +1,14 @@
 import asyncio
 import contextlib
-import logging
 import os
 import shlex
 import subprocess
+import tempfile
 import threading
 
 import typing as t
 
 import aiofiles
-
 
 import tutor.env
 from quart import (
@@ -26,8 +25,6 @@ from tutor import fmt, hooks
 from tutor.types import Config
 import tutor.utils
 from tutor.commands.cli import cli
-
-logger = logging.getLogger(__name__)
 
 
 class TutorProject:
@@ -58,52 +55,50 @@ class TutorProject:
     def _dash_on_config_loaded(cls, config: Config) -> None:
         cls.CONFIG = config
 
-    @classmethod
-    def tutor_stdout_path(cls) -> str:
-        # TODO IMPORTANT remove me
-        return tutor.env.data_path(cls.ROOT, "dash", "tutor.log")
-
 
 class TutorCli:
     """
     Run Tutor commands and capture the output in a file.
 
     Output must be a file because subprocess.Popen requires stdout.fileno() to be
-    available. This file must be unique because it is accessed from a different thread.
+    available. We store This file must be unique because it is accessed from a different thread.
     Basically, the log file is the API between threads.
     """
+
+    INSTANCE: t.Optional["TutorCli"] = None
+
+    def __init__(self, args: list[str]) -> None:
+        self.args = args
+        self.log_path = tempfile.mktemp(prefix="tutor-dash-", suffix=".log")
+        # TODO how do we ensure that the log file is deleted?
+        TutorCli.INSTANCE = self
 
     @classmethod
     def run_parallel(cls, args: list[str]) -> None:
         """
         Run a command in a separate thread.
         """
-        tutor_cli_runner = cls()
-        thread = threading.Thread(
-            target=tutor_cli_runner.run,
-            args=[args],
-        )
+        tutor_cli_runner = cls(args)
+        thread = threading.Thread(target=tutor_cli_runner.run)
         thread.start()
 
-    @property
-    def _log_path(self) -> str:
-        return tutor.env.data_path(TutorProject.ROOT, "dash", "tutor.log")
-
-    def run(self, args: list[str]) -> None:
+    def run(self) -> None:
         """
         Execute some arbitrary tutor command.
 
         Output will be captured in the log file.
         TODO Return the exit code?
         """
-        log_path = self._log_path
-        with open(log_path, "w", encoding="utf8") as stdout:
+        app.logger.info(
+            "Running command: tutor %s (logs: %s)", shlex.join(self.args), self.log_path
+        )
+        with open(self.log_path, "w", encoding="utf8") as stdout:
             # TODO useless because overwritten by Popen
-            stdout.write(f"$ tutor {shlex.join(args)}\n")
+            stdout.write(f"$ tutor {shlex.join(self.args)}\n")
 
         # TODO refactor this ugly mocking
         def _mock_click_echo(text: str, **_kwargs: t.Any) -> None:
-            with open(log_path, "a", encoding="utf8") as stdout:
+            with open(self.log_path, "a", encoding="utf8") as stdout:
                 stdout.write(text)
                 stdout.write("\n")
 
@@ -119,7 +114,7 @@ class TutorCli:
             """
             TODO refactor this
             """
-            with open(log_path, "ab") as stdout:
+            with open(self.log_path, "ab") as stdout:
                 with subprocess.Popen(command, stdout=stdout, stderr=stdout) as p:
                     try:
                         result = p.wait(timeout=None)
@@ -143,9 +138,9 @@ class TutorCli:
         ):
             try:
                 # Call tutor command
-                cli(args)  # pylint: disable=no-value-for-parameter
+                cli(self.args)  # pylint: disable=no-value-for-parameter
             except TutorError as e:
-                with open(log_path, "a", encoding="utf8") as stdout:
+                with open(self.log_path, "a", encoding="utf8") as stdout:
                     stdout.write(e.args[0])
             except SystemExit:
                 # TODO what to do with e.code?
@@ -160,12 +155,11 @@ class TutorCli:
         missed.
         """
         # TODO super ugly. Any way to do better?
-        path = self._log_path
-        if not os.path.exists(path):
+        if not os.path.exists(self.log_path):
             return
-        async with aiofiles.open(path, "r", encoding="utf8") as f:
+        async with aiofiles.open(self.log_path, "r", encoding="utf8") as f:
             while True:
-                if not os.path.exists(path):
+                if not os.path.exists(self.log_path):
                     break
                 content = await f.read()
                 if content:
@@ -215,7 +209,7 @@ async def toggle_plugin(name: str) -> dict[str, str]:
         return {}
 
     # TODO actually toggle plugin
-    logger.info("Toggling plugin %s", name)
+    app.logger.info("Toggling plugin %s", name)
 
     return {}
 
@@ -226,10 +220,10 @@ async def tutor_cli() -> WerkzeugResponse:
     # TODO return 400 if thread is active
     # TODO parse command from JSON request body
     TutorCli.run_parallel(
-        # args=[["dev", "dc", "run", "pouac"]],
+        # ["dev", "dc", "run", "pouac"],
         # ["config", "printvalue", "DOCKER_IMAGE_OPENEDX"],
-        # ["config", "printvalue", "POUAC"],
-        ["local", "launch", "--non-interactive"],
+        ["config", "printvalue", "POUAC"],
+        # ["local", "launch", "--non-interactive"],
     )
     return redirect(url_for("tutor_logs"))
 
@@ -259,12 +253,12 @@ async def tutor_logs() -> str:
 
 @app.websocket("/tutor/logs/stream")
 async def tutor_logs_stream() -> None:
-    tutor_cli_runner = TutorCli()
     while True:
-        async for content in tutor_cli_runner.iter_logs():
-            try:
-                await websocket.send(content)
-            except asyncio.CancelledError:
-                return
+        if TutorCli.INSTANCE:
+            async for content in TutorCli.INSTANCE.iter_logs():
+                try:
+                    await websocket.send(content)
+                except asyncio.CancelledError:
+                    return
         # Exiting the loop means that the file no longer exists, so we wait a little
         await asyncio.sleep(0.1)
