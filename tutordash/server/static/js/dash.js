24356e455d1f7fa45f5c1a953f158ed17acc82ff
Muhammad Labeeb <mlabeeb03@gmail.com>
2025-02-06 15:49:43 +0500

    used flexbox

--- tutordash/server/static/js/dash.js
+++ tutordash/server/static/js/dash.js
@@ -1,7 +1,7 @@
 if (window.location.href.indexOf("/plugin/store") > -1) {
     document.querySelectorAll('.sidebar-tab')[0].classList.toggle('sidebar-tab-selected');
-    document.querySelectorAll('.tab-logo')[0].classList.toggle('sidebar-tab-logo-selected');
+    document.querySelectorAll('.sidebar-tab-logo')[0].classList.toggle('sidebar-tab-logo-selected');
   } else {
     document.querySelectorAll('.sidebar-tab')[1].classList.toggle('sidebar-tab-selected');
-    document.querySelectorAll('.tab-logo')[1].classList.toggle('sidebar-tab-logo-selected');
+    document.querySelectorAll('.sidebar-tab-logo')[1].classList.toggle('sidebar-tab-logo-selected');
   }
