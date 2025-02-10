48e0b689437dfde0acbe9a0cf86379251f1eebd7
Muhammad Labeeb <mlabeeb03@gmail.com>
2025-02-10 16:59:42 +0500

    search functionality in installed plugins

--- tutordash/server/static/js/dash.js
+++ tutordash/server/static/js/dash.js
@@ -5,3 +5,17 @@ if (window.location.href.indexOf("/plugin/store") > -1) {
     document.querySelectorAll('.sidebar-tab')[1].classList.toggle('sidebar-tab-selected');
     document.querySelectorAll('.sidebar-tab-logo')[1].classList.toggle('sidebar-tab-logo-selected');
   }
+
+document.getElementById("search-input").addEventListener("input", function() {
+  let filter = this.value.toLowerCase(); 
+  let plugins = document.querySelectorAll(".installed-plugin"); 
+
+  plugins.forEach(plugin => {
+      let name = plugin.querySelector(".name a").textContent.toLowerCase();
+      if (name.includes(filter)) {
+          plugin.style.display = ""; 
+      } else {
+          plugin.style.display = "none"; 
+      }
+  });
+});
