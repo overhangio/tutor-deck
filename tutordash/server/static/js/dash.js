e980dd2f66e51f5c483c6ad4e13a7728a20a844a
Muhammad Labeeb <mlabeeb03@gmail.com>
2025-02-11 16:46:18 +0500

    use jinja for tab highlighting and remove user info

--- tutordash/server/static/js/dash.js
+++ tutordash/server/static/js/dash.js
@@ -1,11 +1,3 @@
-if (window.location.href.indexOf("/plugin/store") > -1) {
-    document.querySelectorAll('.sidebar-tab')[0].classList.toggle('sidebar-tab-selected');
-    document.querySelectorAll('.sidebar-tab-logo')[0].classList.toggle('sidebar-tab-logo-selected');
-  } else {
-    document.querySelectorAll('.sidebar-tab')[1].classList.toggle('sidebar-tab-selected');
-    document.querySelectorAll('.sidebar-tab-logo')[1].classList.toggle('sidebar-tab-logo-selected');
-  }
-
 document.getElementById("search-input").addEventListener("input", function() {
   let filter = this.value.toLowerCase(); 
   let plugins = document.querySelectorAll(".installed-plugin"); 
