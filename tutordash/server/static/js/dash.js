95b053fd4ae2af7c02ddf73032761a84836985db
Muhammad Labeeb <mlabeeb03@gmail.com>
2025-02-13 13:46:40 +0500

    remove bootstrap

--- tutordash/server/static/js/dash.js
+++ tutordash/server/static/js/dash.js
@@ -1,13 +1,29 @@
-document.getElementById("search-input").addEventListener("input", function() {
-  let filter = this.value.toLowerCase(); 
-  let plugins = document.querySelectorAll(".installed-plugin"); 
+document.getElementById("search-input").addEventListener("input", function () {
+	let filter = this.value.toLowerCase();
+	let plugins = document.querySelectorAll(".installed-plugin");
 
-  plugins.forEach(plugin => {
-      let name = plugin.querySelector(".name a").textContent.toLowerCase();
-      if (name.includes(filter)) {
-          plugin.style.display = ""; 
-      } else {
-          plugin.style.display = "none"; 
-      }
-  });
+	plugins.forEach((plugin) => {
+		let name = plugin.querySelector(".name a").textContent.toLowerCase();
+		if (name.includes(filter)) {
+			plugin.style.display = "";
+		} else {
+			plugin.style.display = "none";
+		}
+	});
+});
+
+let open = document.querySelectorAll(".open_modal_button");
+const modal_container = document.getElementById("modal_container");
+let close = document.querySelectorAll(".close-modal-button");
+
+open.forEach((button) => {
+	button.addEventListener("click", () => {
+		modal_container.classList.add("show");
+	});
+});
+
+close.forEach((button) => {
+	button.addEventListener("click", () => {
+		modal_container.classList.remove("show");
+	});
 });
