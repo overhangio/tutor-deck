ab91336e67385fb4915e553fd83dfb7045f4258f
Muhammad Labeeb <mlabeeb03@gmail.com>
2025-02-18 15:56:02 +0500

    add warning for local launch

--- tutordash/server/static/js/dash.js
+++ tutordash/server/static/js/dash.js
@@ -1,3 +1,17 @@
+function SetWarning(){
+  const warningElements = document.querySelectorAll('[id$="-warning"]');
+  const warningMain = document.getElementById('warning-main');
+  warningElements.forEach(function(warningElement) {
+    const pluginName = warningElement.id.replace('-warning', '');
+    if (document.cookie.includes(pluginName)) {
+        warningElement.style.display = 'block';  
+        warningMain.style.display = 'flex';
+    }
+  });
+}
+SetWarning()
+
+
 let open = document.querySelectorAll(".open-modal-button");
 const modal_container = document.getElementById("modal_container");
 let close = document.querySelectorAll(".close-modal-button");
