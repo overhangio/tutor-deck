18c9240830ae9124b3d3472fb427ef651d16e73b
Muhammad Labeeb <mlabeeb03@gmail.com>
2025-03-14 01:32:26 +0500

    move warning js to proper template

--- tutordash/server/static/js/dash.js
+++ tutordash/server/static/js/dash.js
@@ -1,20 +1,5 @@
-function SetWarning(){
-  const warningElements = document.querySelectorAll('[id^="warning-cookie-"]');
-  const warningMain = document.getElementById('warning-main');
-  warningElements.forEach(function(warningElement) {
-    if (document.cookie.includes(warningElement.id)) {
-        warningElement.style.display = 'flex';  
-        warningMain.style.display = 'flex';
-    }
-  });
-}
-document.body.addEventListener('htmx:afterOnLoad', function(event) {
-  SetWarning();
-});
-
-
-let open = document.querySelectorAll(".open-modal-button");
 const modal_container = document.getElementById("modal_container");
+let open = document.querySelectorAll(".open-modal-button");
 let close = document.querySelectorAll(".close-modal-button");
 
 open.forEach((button) => {
@@ -29,33 +14,32 @@ close.forEach((button) => {
 	});
 });
 
-
 let closeToastButtons = document.querySelectorAll(".close-toast-button");
 const toast = document.querySelector(".toast");
 
 closeToastButtons.forEach((button) => {
-  button.addEventListener("click", () => {
-      hideToast(toast);
-  });
+	button.addEventListener("click", () => {
+		hideToast(toast);
+	});
 });
 
 function showToast(type = "success") {
-  if (toast !== null) {
-    toast.style.display = "flex"; // Show the toast
-    setTimeout(() => {
-      void toast.offsetHeight;
-      toast.classList.add("active");
-    }, 1);
-  }
+	if (toast !== null) {
+		toast.style.display = "flex"; // Show the toast
+		setTimeout(() => {
+			void toast.offsetHeight;
+			toast.classList.add("active");
+		}, 1);
+	}
 }
 
 function hideToast() {
-  if (toast !== null) {
-    toast.classList.remove("active");
+	if (toast !== null) {
+		toast.classList.remove("active");
 
-    // Wait for transition to complete before removing container
-    setTimeout(() => {
-      toast.style.display = "none";
-    }, 500); // Match this to the CSS transition duration
-  }
+		// Wait for transition to complete before removing container
+		setTimeout(() => {
+			toast.style.display = "none";
+		}, 500); // Match this to the CSS transition duration
+	}
 }
