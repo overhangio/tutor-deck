81b82f46657362a24e58828d26dd83fa29d187b7
Muhammad Labeeb <mlabeeb03@gmail.com>
2025-03-21 15:49:44 +0500

    create separate utils

--- tutordash/server/app.py
+++ tutordash/server/app.py
@@ -22,6 +22,8 @@ from quart.helpers import WerkzeugResponse
 from quart.typing import ResponseTypes
 from tutor.plugins.v1 import discover_package
 
+from tutordash.server.utils import current_page_plugins, pagination_context
+
 from . import constants, tutorclient
 
 app = Quart(
@@ -367,25 +369,3 @@ async def command() -> str:
         abort(400, description="Command execution already in progress")
     tutorclient.CliPool.run_parallel(app, command_args)
     return await make_response(redirect(url_for("advanced")))
-
-
-def pagination_context(
-    plugins: list[dict[str, str]], current_page: int
-) -> dict[str, t.Any]:
-    total_pages = (
-        len(plugins) + constants.ITEMS_PER_PAGE - 1
-    ) // constants.ITEMS_PER_PAGE
-    return {
-        "current_page": current_page,
-        "total_pages": total_pages,
-        "previous_page": current_page - 1 if current_page > 1 else None,
-        "next_page": current_page + 1 if current_page < total_pages else None,
-    }
-
-
-def current_page_plugins(
-    plugins: list[dict[str, str]], current_page: int
-) -> list[dict[str, str]]:
-    start_index = (current_page - 1) * constants.ITEMS_PER_PAGE
-    end_index = start_index + constants.ITEMS_PER_PAGE
-    return plugins[start_index:end_index]
